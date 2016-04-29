479c8f92b6756d643f36fdcb877fc4dde0df25aa
Pablo Montoya <pablomontoya392@hotmail.com>
2016-04-29 17:23:46 -0600

    Cambié el input=text por un helper de ruby (form_for f.text_field) y agregué la funcion de autocompletar en base al caché del explorador

--- aplicacion_autored/app/controllers/autored1_controller.rb
+++ aplicacion_autored/app/controllers/autored1_controller.rb
@@ -1,8 +1,13 @@
 class Autored1Controller < ApplicationController
+
   def index
   	@products = Product.all
+    
+    @product_name = Product.name
   	@precio_min = Product.minimum("price")
   	@precio_max = Product.maximum("price")
+    
+    @select_names = Product.uniq.pluck(:name)
   	@select_brands = Product.uniq.pluck(:brand)
   	@select_models = Product.uniq.pluck(:model)
   	@select_years = Product.uniq.pluck(:year)
@@ -13,6 +18,6 @@ class Autored1Controller < ApplicationController
   	@lombardo = 'Lombardo'
   	@calzada = 'Calzada'
   	@navolato = 'Navolato'
-
   end
+
 end
