740fc5c3da110e238d22b6c74150fc699e99368f
Pablo Montoya <pablomontoya392@hotmail.com>
2016-05-02 13:24:49 -0600

    git add -A! Reparé el error de las migraciones

--- aplicacion_autored/app/controllers/autored1_controller.rb
+++ aplicacion_autored/app/controllers/autored1_controller.rb
@@ -1,23 +1,29 @@
 class Autored1Controller < ApplicationController
 
   def index
-  	@products = Product.all
+    #Prueba: mostrar todos los productos en index
+    @products = Product.all
     
-    @product_name = Product.name
-  	@precio_min = Product.minimum("price")
-  	@precio_max = Product.maximum("price")
+    #Prueba: mostrar la variable de precio minimo/maximo en filtros que posteriormente cambiara dependiendo los parámetros de búsqueda
+    @precio_min = Product.minimum("price")
+    @precio_max = Product.maximum("price")
     
+    #Selects para popular los combobox en filtros
     @select_names = Product.uniq.pluck(:name)
-  	@select_brands = Product.uniq.pluck(:brand)
-  	@select_models = Product.uniq.pluck(:model)
-  	@select_years = Product.uniq.pluck(:year)
+    @select_brands = Product.uniq.pluck(:brand)
+    @select_models = Product.uniq.pluck(:model)
+    @select_years = Product.uniq.pluck(:year)
 
-  	@victoria = 'Victoria'
-  	@barrancos = 'Barrancos'
-  	@sanalona = 'Sanalona'
-  	@lombardo = 'Lombardo'
-  	@calzada = 'Calzada'
-  	@navolato = 'Navolato'
+    #Variables de clase para comparar resultados y mostrar diferente información dependiendo la búsqueda. (google maps)
+    @victoria = 'Victoria'
+    @barrancos = 'Barrancos'
+    @sanalona = 'Sanalona'
+    @lombardo = 'Lombardo'
+    @calzada = 'Calzada'
+    @navolato = 'Navolato'
+
+    #Variable de clase para el tex_field del layout
+    @product_name = Product.name
   end
 
 end
